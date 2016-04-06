25cd54a9883061911abd444ef61f0e9af24699d4
Mikhail Kotelnikov <mikhail.kotelnikov@gmail.com>
2016-04-06 13:52:02 +0200

    Update MapnikConfigLoader and MapnikRenderer classes. Add the CustomTilesHandler class.

--- index.js
+++ index.js
@@ -3,4 +3,8 @@ module.exports = {
     MapnikConfigLoader : require('./lib/MapnikConfigLoader'),
     MapnikRenderer : require('./lib/MapnikRenderer'),
     RendererLoader : require('./lib/RendererLoader'),
+    Utils : require('./lib/Utils'),
+    
+    CustomTilesHandler : require('./lib/CustomTilesHandler'),
+    TilesHandler : require('./lib/TilesHandler'),
 };
