14774ee49af66506ef727a2d46045cee101e61d4
Mikhail Kotelnikov <mikhail.kotelnikov@gmail.com>
2016-04-12 16:20:22 +0200

    Export tile providers

--- index.js
+++ index.js
@@ -4,7 +4,18 @@ module.exports = {
     MapnikRenderer : require('./lib/MapnikRenderer'),
     RendererLoader : require('./lib/RendererLoader'),
     Utils : require('./lib/Utils'),
-    
+
     TilesHandler : require('./lib/TilesHandler'),
     GeoJsonTilesHandler : require('./lib/GeoJsonTilesHandler'),
+
+    CompositeTilesProvider : require('./lib/providres/CompositeTilesProvider'),
+    DispatchingTilesProvider : require('./lib/providres/DispatchingTilesProvider'),
+    MapnikTilesProvider : require('./lib/providres/MapnikTilesProvider'),
+    parseLayersInfo : require('./lib/providres/parseLayersInfo'),
+    TilesProvider : require('./lib/providres/TilesProvider'),
+    VectorTilesDeserializer : require('./lib/providres/VectorTilesDeserializer'),
+    VectorTilesGenerator : require('./lib/providres/VectorTilesGenerator'),
+    VectorTilesSerializer : require('./lib/providres/VectorTilesSerializer'),
+    VectorTilesToImage : require('./lib/providres/VectorTilesToImage'),
+    VectorTilesToUtfGrid : require('./lib/providres/VectorTilesToUtfGrid'),
 };
