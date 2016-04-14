b7fb3c63a9f1658d40cc0d7c869850205138fbd7
Mikhail Kotelnikov <mikhail.kotelnikov@gmail.com>
2016-04-14 21:50:40 +0200

    Move the UtfGridMerge class in the "providers" package.

--- test/data/project4/style/project.js
+++ test/data/project4/style/project.js
@@ -2,15 +2,14 @@ module.exports = function() {
     return {
 //        "srs" : "+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +no_defs +over",
 //        "srs" : "+init=epsg:3857",
+        "srs" : "+init=epsg:3857",
         "Stylesheet" : [ {
             'Map' : {
-                'background-color' : 'cyan'
+                'background-color' : 'white'
             },
             '#basemap' : {
-                'polygon-fill' : '#f2eff9',
-                'polygon-opacity' : 1,
-                'line-width' : 0.1,
-                'line-color' : '#426',
+                'line-width' : 1,
+                'line-color' : 'red',
             }
         } ],
         "Layer" : [ {
--- test/providers/GeoJsonToPbfTest.js
+++ test/providers/GeoJsonToPbfTest.js
@@ -4,7 +4,8 @@ var GeoJsonToPbf = require('../../lib/providers/GeoJsonToPbf');
 var VectorTilesToImage = require('../../lib/providers/VectorTilesToImage');
 var VectorTilesDeserializer = require('../../lib/providers/VectorTilesDeserializer');
 var RendererLoader = require('../../lib/RendererLoader');
-
+var MapnikConfigLoader = require('../../lib/MapnikConfigLoader');
+var MapnikRenderer = require('../../lib/MapnikRenderer');
 var Utils = require('../../lib/Utils');
 var extend = Utils.extend;
 var TestUtils = require('../TestUtils');
@@ -12,61 +13,97 @@ var TestUtils = require('../TestUtils');
 describe('GeoJsonToPbfTest', function() {
 
     TestUtils.runTest('generates image map tiles', function() {
-        var projectDir = TestUtils.getPath('project4');
-        var source = new GeoJsonToPbf({
-            data : {
-                'basemap' : projectDir + '/world.geo.json'
-            }
-        });
-        var toVectorTile = new VectorTilesDeserializer();
-        var renderer = new VectorTilesToImage({
-            loader : new RendererLoader({
-                baseDir : projectDir,
-                tileSize : 512
-            })
-        });
+        var projectDir = TestUtils.getPath('project3');
+        // var toVectorTile = new VectorTilesDeserializer();
+        // var renderer = new VectorTilesToImage({
+        // loader : new RendererLoader({
+        // baseDir : projectDir,
+        // tileSize : 512
+        // })
+        // });
 
-        var options = {
-            params : {
-                x : 16,
-                y : 11,
-                z : 5,
-                format : 'svg'
-            }
-        };
         return Promise.resolve()//
         .then(function() {
-            return source.loadTile(options).then(function(result) {
-                expect(result.params).to.eql(options.params);
-                expect(result.tile instanceof Buffer).to.be(true);
-                expect(result.headers).to.eql({
-                    'Content-Type' : 'application/x-protobuf'
-                });
-                return result;
+            var loader = new MapnikConfigLoader();
+            return loader.readProject({
+                dir : projectDir + '/style'
             });
-        }).then(function(result) {
-            // Deserialize the buffer
-            return toVectorTile.loadTile(result)//
-            .then(function(options) {
-                // Render the VectorTile object
-                return renderer.loadTile(options);
+        }).then(function(params) {
+            var renderer = new MapnikRenderer({
+                xml : params.xml,
+                tileSize : 512
             });
-        }).then(function(result) {
+            return renderer;
+        }).then(function(renderer) {
+            var dataFile = projectDir + //
+            '/source/layers/basemap/world.geo.json';
+            var source = new GeoJsonToPbf({
+                data : {
+                    'basemap' : dataFile
+                }
+            });
+            var options = {
+                params : {
+                    x : 0,
+                    y : 0,
+                    z : 0,
+                    format : 'png'
+                }
+            };
+            return Promise.resolve().then(function() {
+                return source.loadTile(options);
+            }).then(function(result) {
+                
+                return renderer.loadVectorTile(result.tile, options.params)//
+                
+                console.log('??? ', result);
+                return renderer.loadVectorTile(result.tile, options.params)//
+            }).then(function(res) {
+                return renderer.renderVectorTile(res);
+            });
+        }).then(function(tile) {
+            var control = fs.readFileSync(projectDir + '/map.pdf');
             var params = options.params;
-            expect(result.params).to.eql(params);
-            expect(result.tile instanceof Buffer).to.be(true);
-            expect(result.headers).to.eql({
-                'Content-Type' : 'image/png'
-            });
-
             var suffix = [ params.z, params.x, params.y ].join('-');
             var file = '/tile-' + suffix + '.png';
-            var control = fs.readFileSync(projectDir + file);
-            fs.writeFileSync(__dirname + file, result.tile);
-             console.log('???', __dirname + file);
-//            expect(result.tile.toString('base64'))//
-//            .to.eql(control.toString('base64'));
+            fs.writeFileSync(__dirname + file, tile);
+
+            expect(tile.toString('base64')).//
+            to.eql(control.toString('base64'));
         });
+
+        //
+        // .then(function() {
+        // expect(result.params).to.eql(options.params);
+        // expect(result.tile instanceof Buffer).to.be(true);
+        // expect(result.headers).to.eql({
+        // 'Content-Type' : 'application/x-protobuf'
+        // });
+        // return result;
+        // });
+        // }).then(function(result) {
+        // // Deserialize the buffer
+        // return toVectorTile.loadTile(result)//
+        // .then(function(options) {
+        // // Render the VectorTile object
+        // return renderer.loadTile(options);
+        // });
+        // }).then(function(result) {
+        // var params = options.params;
+        // expect(result.params).to.eql(params);
+        // expect(result.tile instanceof Buffer).to.be(true);
+        // expect(result.headers).to.eql({
+        // 'Content-Type' : 'image/png'
+        // });
+        //
+        // var suffix = [ params.z, params.x, params.y ].join('-');
+        // var file = '/tile-' + suffix + '.png';
+        // var control = fs.readFileSync(projectDir + file);
+        // fs.writeFileSync(__dirname + file, result.tile);
+        // console.log('???', __dirname + file);
+        // // expect(result.tile.toString('base64'))//
+        // // .to.eql(control.toString('base64'));
+        // });
     });
 
 });
