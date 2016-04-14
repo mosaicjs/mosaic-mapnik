128d135aea7297c0df6d701908e1d85e1165cffb
Mikhail Kotelnikov <mikhail.kotelnikov@gmail.com>
2016-04-14 22:06:59 +0200

    Code re-organization

--- index.js
+++ index.js
@@ -1,22 +1,7 @@
 module.exports = {
-    CartoCssSerializer : require('./lib/CartoCssSerializer'),
-    MapnikConfigLoader : require('./lib/MapnikConfigLoader'),
-    MapnikRenderer : require('./lib/MapnikRenderer'),
-    RendererLoader : require('./lib/RendererLoader'),
-    Utils : require('./lib/Utils'),
-
-    TilesHandler : require('./lib/TilesHandler'),
-    GeoJsonTilesHandler : require('./lib/GeoJsonTilesHandler'),
-
-    CompositeTilesProvider : require('./lib/providers/CompositeTilesProvider'),
-    DispatchingTilesProvider : require('./lib/providers/DispatchingTilesProvider'),
-    GeoJsonToPbf : require('./lib/providers/GeoJsonToPbf'),
-    MapnikTilesProvider : require('./lib/providers/MapnikTilesProvider'),
-    parseLayersInfo : require('./lib/providers/parseLayersInfo'),
-    TilesProvider : require('./lib/providers/TilesProvider'),
-    VectorTilesDeserializer : require('./lib/providers/VectorTilesDeserializer'),
-    VectorTilesGenerator : require('./lib/providers/VectorTilesGenerator'),
-    VectorTilesSerializer : require('./lib/providers/VectorTilesSerializer'),
-    VectorTilesToImage : require('./lib/providers/VectorTilesToImage'),
-    VectorTilesToUtfGrid : require('./lib/providers/VectorTilesToUtfGrid'),
+    handlers : require('./lib/handlers'),
+    loader : require('./lib/loader'),
+    renderer : require('./lib/renderer'),
+    tiles : require('./lib/tiles'),
+    Utils : require('./lib/Utils')
 };
